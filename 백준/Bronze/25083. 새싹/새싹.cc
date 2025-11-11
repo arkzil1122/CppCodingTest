4c52fecacdf714c82ce6c4ace9ef93ffe6ab7007
arkzil1122 <89529876+arkzil1122@users.noreply.github.com>
2025-11-11 23:47:32 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// 코드 작성
+	/*
+	         ,r'"7
+	r`-_   ,'  ,/
+	 \. ". L_r'
+	   `~\/
+	      |
+	      |
+	*/
+
+	cout << "         ,r'\"7" << endl;
+	cout << "r`-_   ,'  ,/" << endl;
+	cout << " \\. \". L_r'" << endl;
+	cout << "   `~\\/" << endl;
+	cout << "      |" << endl;
+	cout << "      |" << endl;	
+	return 0;
+}
