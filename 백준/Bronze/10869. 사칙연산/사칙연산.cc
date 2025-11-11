6dd49ce381333716d7cbedf3efacc22aaf36d4d3
arkzil1122 <89529876+arkzil1122@users.noreply.github.com>
2025-11-11 23:46:53 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// 코드 작성
+	int a, b;
+
+	cin >> a >> b;
+
+	cout << (a + b) << endl;
+	cout << (a - b) << endl;
+	cout << (a * b) << endl;
+	cout << (a / b) << endl;
+	cout << (a % b) << endl;
+	return 0;
+}
